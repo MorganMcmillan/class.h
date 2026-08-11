92525e1ed1c9561b3ef1cb2756f47c10196c9277
morganmcmillan <mcmillan.a.morgan@gmail.com>
2026-08-11 12:30:34 -0600

    Feat: add interfaces

--- class.h
+++ class.h
@@ -1,40 +1,65 @@
 #ifndef CLASS_H
 #define CLASS_H
 
+// Note: this library makes liberal use of an implicit self.
+
+// Semantic wrapper for defining a class
 #define class(Cls, fields) typedef struct fields Cls;
 
 // Defines a class method
 // Expected to be preceded by the return type, and followed by a {} function
-// body
-#define method(Cls, name, ...) Cls##_##name(self *Cls, __VA_ARGS__)
+// body.
+#define method(name, Self, ...) Self##_##name(Self *self, __VA_ARGS__)
+
+// Defines a class method without any extra arguments.
+#define method0(name, Self) Self##_##name(Self *self)
 
 // Defines a class constructor
 // Expected to be followed by a {} function body
 #define constructor(Cls, ...) Cls Cls##_create(__VA_ARGS__)
 
-#define ivirtual(name, Interface, ...) (*name)(Interface * self, ...)
+// Declares/defines a virtual method for a class-vtable pair.
+#define virtual(name, ...) (*name)(void *self, __VA_ARGS__)
 
-#define virtual(name, ...) (*name)(void *self, ...)
+// Declares/defines a virtual method for a class-vtable pair, without any
+// arguments.
+#define virtual0(name) (*name)(void *self)
 
-#define vcall(self, name, ...) (*self->vtable->name)(self, __VA_ARGS__)
+// Casts a function to a virtual table entry.
+#define vcast(name, ret_type, ...) (ret_type (*)(void *, __VA_ARGS__)) name
 
-#define impl(Cls, Interface, vtable)                                           \
-  Interface Cls##_##Interface##_vtable = vtable
+// Casts a function to a virtual table entry, without extra arguments.
+#define vcast0(name, ret_type) (ret_type (*)(void *)) name
 
-// Creates a class-vtable pair to represent a polymorphic instance of an
-// interface.
-#define def_dyn(Cls, Interface)                                                \
-  typedef struct {                                                             \
-    Cls value;                                                                 \
-    Interface vtable                                                           \
-  } Cls##_##Interface
+#define vcall(self, name, ...)                                                 \
+    (*self->vtable->name)((void *)self->super, __VA_ARGS__)
 
-#define dyn(Cls, Interface, name, val)                                         \
-  Cls##_##Interface name = (Cls##_##Interface) {                               \
-    .value = val, .vtable = Cls##_##Interface##_vtable                         \
-  }
+#define vcall0(self, name) (*self->vtable->name)((void *)self->super)
 
-// Explicitly downcasts one pointer type to another
-#define downcast(name, Cls, new_name) Cls *new_name = (Cls *)name
+// Defines an interface as a class-vtable pointer pair.
+#define interface(Interface, vtable_def)                                       \
+    typedef struct vtable_def Interface##_vtable;                              \
+    typedef struct {                                                           \
+        void *super;                                                           \
+        Interface##_vtable *vtable;                                            \
+    } Interface;
+
+// Defines an implementation of an interface.
+#define impl(Cls, Interface, vtable_impl)                                      \
+    Interface##_vtable Cls##_##Interface##_vtable =                            \
+        (Interface##_vtable)vtable_impl
+
+#define dyn(Interface, Cls, value)                                             \
+    (Interface) { .super = value, .vtable = &Cls##_##Interface##_vtable }
+
+// Explicitly downcasts this instance to its superclass.
+#define downcast(name) &(name->super)
+
+// Explicitly downcasts one pointer type to another.
+#define downcast_ptr(Cls, name) (Cls *)name
+
+// Explicitly downcasts one pointer type to another, and assigns it to a new
+// variable.
+#define downcast_declare(Cls, name, new_name) Cls *new_name = (Cls *)name
 
 #endif /* CLASS_H */
---
+++ example.c
@@ -0,0 +1,75 @@
+#include "class.h"
+#include <math.h>
+#include <stdio.h>
+#include <sys/select.h>
+
+class(Vec2, {
+    int x;
+    int y;
+});
+
+constructor(Vec2, int x, int y) { return (Vec2){x, y}; }
+
+Vec2 method(add, Vec2, Vec2 *other) {
+    return Vec2_create(self->x + other->x, self->y + other->y);
+}
+
+Vec2 method(sub, Vec2, Vec2 *other) {
+    return Vec2_create(self->x - other->x, self->y - other->y);
+}
+
+Vec2 method(scale, Vec2, int n) {
+    return Vec2_create(self->x * n, self->y * n);
+}
+
+double method0(distance, Vec2) {
+    return sqrt((double)(self->x * self->x + self->y * self->y));
+}
+
+class(Vec3, {
+    Vec2 super;
+    int z;
+});
+
+constructor(Vec3, int x, int y, int z) {
+    return (Vec3){
+        Vec2_create(x, y),
+        z,
+    };
+}
+
+Vec3 Vec3_from_vec2(Vec2 super, int z) { return (Vec3){super, z}; }
+
+Vec3 method(add, Vec3, Vec3 *other) {
+    return Vec3_from_vec2(Vec2_add(downcast(self), downcast(other)),
+                          self->z + other->z);
+}
+
+double method0(distance, Vec3) {
+    return sqrt((double)(self->super.x * self->super.x +
+                         self->super.y * self->super.y + self->z * self->z));
+}
+
+interface(Distance, { double virtual0(distance); });
+
+double method0(distance, Distance) { return vcall0(self, distance); }
+
+impl(Vec2, Distance, {vcast0(Vec2_distance, double)});
+
+impl(Vec3, Distance, {vcast0(Vec3_distance, double)});
+
+int main() {
+    Vec2 vec2 = Vec2_create(5, 10);
+    printf("Vec2 distance: %f\n", Vec2_distance(&vec2));
+    Vec3 vec3 = Vec3_from_vec2(vec2, 15);
+    printf("Vec3 distance: %f\n", Vec3_distance(&vec3));
+
+    Distance distances[2] = {
+        dyn(Distance, Vec2, &vec2),
+        dyn(Distance, Vec3, &vec3),
+    };
+
+    for (int i = 0; i < 2; i++) {
+        printf("Distance: %f\n", Distance_distance(&distances[i]));
+    }
+}
